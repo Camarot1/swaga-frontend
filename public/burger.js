08d4e5900f912e0f3645570d930bf6abd8940393
Camarot1 <tupojdoter@gmail.com>
2026-05-17 23:05:34 +0300

    fix burger to multi browser

--- public/burger.js
+++ public/burger.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let burger = document.querySelector(".header__burger")
     let menu = document.querySelector(".header__navigation")
     let logo = document.querySelector(".header__name")
+    if (!burger || !menu || !logo) return
     if (window.screen.width <= 900) {
         menu.classList.add("hidden")
     }
